b8f9235e7f8a6bf28020ed9421736cef38d343b7
vivihm <viviana.marca@sptech.school>
2023-03-13 19:40:06 -0300

    conexão das página e ajustes do css

--- site/public/js/funcoes.js
+++ site/public/js/funcoes.js
@@ -1,10 +1,31 @@
-    function MostrarMenu() {
-        let menuMobile = document.querySelector('.mobile-menu');
+window.onscroll = function () { scrollFunction() };
 
-        if (menuMobile.classList.contains('open')) {
-            menuMobile.classList.remove('open');
-        }else{
-            menuMobile.classList.add('open');
-        }
-    
-        }
+function scrollFunction() {
+    if (document.body.scrollTop > 1400 || document.documentElement.scrollTop > 1400) {
+        document.getElementById("quem_somos_selecao").style.color = "#000"
+        document.getElementById("servicos_selecao").style.color = "#000"
+        document.getElementById("contato_selecao").style.color = "#6B0CC5"
+
+    } else if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200){
+        document.getElementById("quem_somos_selecao").style.color = "#000"
+        document.getElementById("servicos_selecao").style.color = "#6B0CC5"
+        document.getElementById("contato_selecao").style.color = "#000"
+
+    } else if(document.body.scrollTop > 0 || document.documentElement.scrollTop > 0){
+
+        document.getElementById("quem_somos_selecao").style.color = "#6B0CC5"
+        document.getElementById("servicos_selecao").style.color = "#000"
+        document.getElementById("contato_selecao").style.color = "#000"
+    }
+}
+
+function MostrarMenu() {
+    let menuMobile = document.querySelector('.mobile-menu');
+
+    if (menuMobile.classList.contains('open')) {
+        menuMobile.classList.remove('open');
+    } else {
+        menuMobile.classList.add('open');
+    }
+
+}
