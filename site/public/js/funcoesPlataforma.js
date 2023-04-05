981b40a37358baebd1737f3d712aea259a2a29ba
vivihm <viviana.marca@sptech.school>
2023-04-05 14:54:59 -0300

    Alterações no menu lateral

--- site/public/js/funcoesPlataforma.js
+++ site/public/js/funcoesPlataforma.js
@@ -1,15 +1,3 @@
-// função para a página de dashboard momentaneo, pois isso deve mudar ao clicar e nao passar o mouse!!!!
-    let lista = document.querySelectorAll('.menu-lateral a');
-
-    function ativarLink() {
-        lista.forEach((item) => item.classList.remove('hovered'));
-        this.classList.add('hovered');
-    }
-
-    lista.forEach((item) => {
-        item.addEventListener('mouseover', ativarLink);
-    });
-
 
 // menu sanduiche
 // ele adiciona o a classe 'ativado' nas seguintes classes
