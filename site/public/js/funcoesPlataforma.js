4b32975b41d9bd41859d1cebde1cfb1e8546e7ab
vivihm <viviana.marca@sptech.school>
2023-04-08 10:34:50 -0300

    Alterações na função do menu e correçoes no css

--- site/public/js/funcoesPlataforma.js
+++ site/public/js/funcoesPlataforma.js
@@ -1,11 +1,23 @@
 
 // menu sanduiche
-// ele adiciona o a classe 'ativado' nas seguintes classes
 let menuSanduiche = document.querySelector('.abre-fecha-menu');
+// ele adiciona o a classe 'ativado' nas seguintes classes
 let navbar = document.querySelector('.navbar');
 let conteudo = document.querySelector('.conteudo');
+let logo = document.querySelector(".imagem_logo")
 
-menuSanduiche.onclick = function(){
+
+menuSanduiche.onclick = function () {
     navbar.classList.toggle('ativado');
     conteudo.classList.toggle('ativado');
+    logo.classList.toggle("ativado");
+
+    let logoAtivado = document.querySelector(".imagem_logo.ativado")
+
+    if (logoAtivado) {
+        logoAtivado.src = "../assets/dashboard/icone-logo.png";
+    }else{   
+        logo.src = "../assets/HEMERA.png";
+    }
+
 }
